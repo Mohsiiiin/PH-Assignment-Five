100fbc4bba476812bd509cbd31ac7cbfd1fb294b
mohsiiiin <ahmedmohsiiiin@gmail.com>
2025-12-04 19:58:51 +0600

    navbar heart count implement done

--- index.js
+++ index.js
@@ -8,4 +8,18 @@
 // heart-click
 // call-button
 // copy-button
-console.log('js connected')
+
+// heart-count-implement-code...............
+
+const heartCount = document.getElementById("heart-count");
+
+const hearts = document.getElementsByClassName("heart-click");
+
+let count = 0;
+
+for (let heart of hearts){
+    heart.addEventListener('click', function(){
+        heartCount.innerText = count;
+        count ++;
+    })
+}
