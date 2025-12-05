9077d27fd21b9ba649cadad97dbc03a6b29ab1a1
mohsiiiin <ahmedmohsiiiin@gmail.com>
2025-12-05 15:17:45 +0600

    call alert or coin- function added

--- index.js
+++ index.js
@@ -1,10 +1,12 @@
-// id.......
+// All-id.......
 // heart-count
 // coins-count
 // copy-count
+// history-list
 // clear-btn
 
-// class.......
+
+// All-class.......
 // heart-click
 // call-button
 // copy-button
@@ -23,3 +25,43 @@ for (let heart of hearts){
         count ++;
     })
 }
+
+// Dom-element gula dore niye ashlam......
+
+// coins-count......
+const coinsCount = document.getElementById('coins-count');
+
+// all call-buttons.....
+const callButtons = document.getElementsByClassName('call-button');
+
+// call history-list.....
+const historyList = document.getElementById('history-list');
+
+// clear history-button....
+const clearBtn = document.getElementById('clear-btn');
+
+// cards-call&coins function.....
+for (let callButton of callButtons){
+    callButton.addEventListener('click', function(){
+        let coins = parseInt(coinsCount.innerText)
+        if(coins < 20 ){
+            alert('You do not have enough coins the call will cost at least 20 coins.')
+            return;
+        }
+
+        coins = coins - 20;
+        coinsCount.innerText = coins;
+
+        let card = callButton.parentElement.parentElement.parentElement;
+        
+        const allH1 = card.querySelectorAll('h1');
+
+        const subTitle = allH1[0].innerText;
+        const subNmbr = allH1[1].innerText;
+
+        alert('calling'+ subTitle + ' ' + subNmbr);
+
+    })
+}
+
+
